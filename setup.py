f8b21e6837ce183efda88bcdbe1ac319066868b0
rlhjansen <rlh.jansen@outlook.com>
2019-08-18 00:08:51 +0100

    reordering...

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=[],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.1'
     __ver__='0.1'
     # The license can be anything you like
     license='MIT',
