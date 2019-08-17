506a1dfbd5ad7db08129170d0e3ea9bf714751e0
rlhjansen <rlh.jansen@outlook.com>
2019-08-18 00:41:53 +0100

    fixing version number issues

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup
-import re, io
 
-__version__ = re.search(
-    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
-    io.open('personal_util/__init__.py', encoding='utf_8_sig').read()
-    ).group(1)
 # The beautiful part is, I don't even need to check exceptions here.
 # If something messes up, let the build process fail noisy, BEFORE my release!
-
-print(__version__)
 
 setup(
      # Needed to silence warnings (and to be a worthwhile package)
@@ -22,7 +15,7 @@ setup(
     install_requires=[],
     # The license can be anything you like
     license='MIT',
-    version=__version__,
+    version=0.1,
     description="""small library for functions I use over and over again.
     Will probably split into different modules in the future""",
     # We will also need a readme eventually (there will be a warning)
