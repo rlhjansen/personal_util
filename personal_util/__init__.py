78d677714a88e869869db747362e72524054d302
rlhjansen <rlh.jansen@outlook.com>
2019-08-18 00:16:25 +0100

    reordering...

--- personal_util/__init__.py
+++ personal_util/__init__.py
@@ -1,8 +1,1 @@
-from pkg_resources import get_distribution
-
 from .my_stuff import make_requirements, lprint
-
-__version__ = get_distribution('personal_util').version
-print(__version__)
-print("100% yeet")
-input()
